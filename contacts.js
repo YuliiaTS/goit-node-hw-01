ba4cea0716792c3a795a76732ad7f83243768b49
Yuliia Kovalchuk <iuliia1702@gmail.com>
2022-10-01 20:08:25 +0300

    start files

---
+++ contacts.js
@@ -0,0 +1,32 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+
+    return contacts;
+};
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+const contacts = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
+
+module.exports = contacts;
